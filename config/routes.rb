94d3082e84de127919fd96444c3d67ac70cd35f1
Jason Bishop <devilshaircut@gmail.com>
2011-09-29 00:06:48 -0400

    Fix some images and add in left/right sidebar template pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Cssframework::Application.routes.draw do
   root :to => "home#index"
   match 'pages/shortcodes' => "pages#shortcodes"
   match 'pages/empire' => "pages#empire"
+  match 'pages/empire-right-sidebar' => "pages#empire-right-sidebar"
+  match 'pages/empire-left-sidebar' => "pages#empire-left-sidebar"  
 
 end
